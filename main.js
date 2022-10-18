ac58693ebae887a862ff42f915d3204c7ac9a4e8
ManhCuong2706 <manhcuong2132@gmail.com>
2022-10-18 12:14:39 +0700

    update on 18/10/2022

---
+++ main.js
@@ -0,0 +1,11 @@
+import myBigNum from './myBigNum';
+
+const $ = (s) => {
+  return document.querySelector(s);
+};
+
+const numA = $('#numA');
+const numB = $('#numB');
+
+const btnAdd = $('#btnAdd');
+const result = $('#result');
---
+++ myBigNum.js
@@ -0,0 +1,79 @@
+const $ = (s) => {
+  return document.querySelector(s);
+};
+
+const numA = $('#numA');
+const numB = $('#numB');
+const logWrap = $('#logWrap');
+
+const btnAdd = $('#btnAdd');
+
+const renderLog = (logData) => {
+  let html = logData.map((log) => {
+    return `<p>${log}</p></p>`;
+  });
+  logWrap.innerHTML = html.join('\n');
+};
+
+const myBigNum = (A, B) => {
+  const lenA = A.length;
+  const lenB = B.length;
+  const log = [];
+
+  let temp = 0;
+  let mind = 0;
+  let r = 0;
+  const result = [];
+
+  if (lenA > lenB) {
+    let len = lenA - lenB;
+    for (let i = 0; i < len; i++) {
+      B = 0 + B;
+    }
+  } else {
+    let len = lenB - lenA;
+    for (let i = 0; i < len; i++) {
+      A = 0 + A;
+    }
+  }
+
+  if (lenA > lenB) {
+    for (let i = lenA - 1; i >= 0; i--) {
+      temp = Number(A[i]) + Number(B[i]) + mind;
+      log.push(`${A[i]} + ${B[i]} = ${temp}`);
+      mind = Math.trunc(temp / 10);
+      log.push(`Nhớ ${mind}`);
+
+      r = temp % 10;
+      result.unshift(r);
+      log.push(`Kết quả : ${result.join('')}`);
+    }
+    if (mind > 0) {
+      temp = Number(A[i]) + Number(B[i]) + mind;
+    }
+  } else {
+    for (let i = lenB - 1; i >= 0; i--) {
+      temp = Number(A[i]) + Number(B[i]) + mind;
+      log.push(`${A[i]} + ${B[i]} = ${temp}`);
+
+      mind = Math.trunc(temp / 10);
+      log.push(`Nhớ ${mind}`);
+
+      r = temp % 10;
+      result.unshift(r);
+      log.push(`Kết quả : ${result.join('')}`);
+    }
+    if (mind > 0) {
+      temp = Number(A[i]) + Number(B[i]) + mind;
+    }
+  }
+  renderLog(log);
+  return result.join('');
+};
+
+btnAdd.onclick = () => {
+  console.log('click');
+  const numAValue = numA.value;
+  const numBValue = numB.value;
+  myBigNum(numAValue, numBValue);
+};
