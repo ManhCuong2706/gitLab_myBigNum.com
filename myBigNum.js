e712fc6427e044246ff85d48a3f0ccb7bfd3583d
ManhCuong2706 <manhcuong2132@gmail.com>
2022-10-19 13:34:39 +0700

    upload

--- myBigNum.js
+++ myBigNum.js
@@ -2,7 +2,6 @@ const myBigNum = (A, B) => {
   const lenA = A.length;
   const lenB = B.length;
   const maxLen = Math.max(lenA, lenB);
-
   const log = [];
 
   let temp = 0;
@@ -10,12 +9,18 @@ const myBigNum = (A, B) => {
   let r = 0;
   const result = [];
 
-  for (let i = 0; i < maxLen; i++) {
-    let i1 = lenA - i - 1;
-    let i2 = lenB - i - 1;
+  let i1 = 0;
+  let i2 = 0;
 
-    let c1 = i1 < 0 ? 0 : Number(A[i1]);
-    let c2 = i2 < 0 ? 0 : Number(B[i2]);
+  let c1 = 0;
+  let c2 = 0;
+
+  for (let i = 0; i < maxLen; i++) {
+    i1 = lenA - i - 1;
+    i2 = lenB - i - 1;
+
+    c1 = i1 < 0 ? 0 : Number(A[i1]);
+    c2 = i2 < 0 ? 0 : Number(B[i2]);
 
     temp = c1 + c2 + mind;
 
@@ -30,5 +35,4 @@ const myBigNum = (A, B) => {
     log.push(`Kết quả : ${result.join('')}`);
   }
   renderLog(log);
-  return result.join('');
 };
